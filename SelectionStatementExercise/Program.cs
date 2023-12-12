062f8999922dd07dba0204b4d5fda66d8a61d99e
Philip Kim <filshouse56@gmail.com>
2023-12-12 12:38:09 -0500

    Ecercise completed

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GuessNumber();
         }
+
+        public static void GuessNumber()
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 100);
+
+            Console.WriteLine("Choose a number between 1 - 100");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput == 37)
+            {
+                Console.WriteLine("You are correct. Great Job!!!");
+            }
+            else if (userInput <= 36) 
+            {
+                Console.WriteLine("Too low, Try again");
+             
+            }
+            else if (userInput >=38) 
+            {
+                Console.WriteLine("Too High. Try again");
+            }
+
+        }
+    
+
     }
 }
